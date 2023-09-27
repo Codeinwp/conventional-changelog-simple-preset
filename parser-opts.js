0f03ff541ac38b890b90fcc9dd3170ebdee21f1e
Marius Cristea <marius.cristea@vertistudio.com>
2023-09-27 10:15:48 +0300

    Update parser-opts.js
    Fix pattern match for release:fixes, which was not working

--- parser-opts.js
+++ parser-opts.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  headerPattern: /^(release)(?:\((.*)\))?: (.*)$/,
+  headerPattern: /^(release)(?:\((.*)\))?:(.*)$/,
   headerCorrespondence: [
     'type',
     'scope',
